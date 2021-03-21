7489937a2fcc8074106a9f177e9282817e64cf8c
himajin <42849550+himajin-no-tameiki@users.noreply.github.com>
2021-03-21 16:27:45 +0900

    add instructions

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="container">
-    <div class="content">
-      <p>
-        If you disable the color of a layer, the <b>shadow color</b> of the style of
-        each line will be used. You can abuse this to make the color style-dependent.
-      </p>
-    </div>
     <div class="field has-addons">
       <p class="control">
         <button @click="importSettings" class="button is-small">Import...</button>
@@ -33,6 +27,38 @@
     </div>
     <div class="field">
       <button @click="applyLayers" :disabled="processing" class="button is-info">Apply Layers</button>
+    </div>
+    <div class="content">
+      <details>
+        <summary>What does this program do?</summary>
+        <p>
+          This program helps you create a complex style for .ass subtitles.
+          What's great about the ASS format is you can put subtitles on different layers,
+          which this app abuses to create a multi-layer, rich style.
+        </p>
+        <p>
+          Every layer in the above table adds an outline, and higher index layers are placed
+          <b>above</b> lower index ones. You can for example add a black layer at the bottom,
+          add some blur and increase Offset Y a bit, to make a nice, realistic drop shadow.
+          Or you can add a blurred layer with a light color at the bottom to make it glow.
+        </p>
+        <p>
+          You can hit the "Show Preview" button to show an preview of your style, and it updates
+          automatically as you tweak the layer options.
+        </p>
+        <p>
+          If you disable the color of a layer by unticking the checkbox, the <b>shadow color</b>
+          of the style of each line will be used. You can abuse this to make the color style-dependent
+          (by setting different shadow colors Aegisub).
+        </p>
+        <p>
+          When you're done tweaking, you can apply the style on an existing ASS file, by first
+          selecting input and output files, and then hitting "Apply Layers" button. This will duplicate
+          each line in the source file into as many layers as you specified, into the output file. This
+          means you should do this only after you're done inputting all lines in Aegisub (or
+          whatever subtitle software) and ready to burn them in into a video.
+        </p>
+      </details>
     </div>
   </div>
 </template>
